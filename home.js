97ef75ba57fa326141251defddf1460f76189570
Chroma165 <davidleutert@icloud.com>
2025-09-04 19:54:09 +0200

    Got express and socket.io to work. Frontend now properly connects to backend

---
+++ home.js
@@ -0,0 +1,5 @@
+const socket = io();
+
+const createRoom = document.querySelector('#createRoom');
+const joinRoom = document.querySelector('#joinRoom');
+const roomCodeInput = document.querySelector('#roomCodeInput');
--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const { request } = require('http');
 
 const app = express();
 
+app.use(express.static(__dirname));
+
+const PORT = process.env.PORT || 3000;
+const appServer = app.listen(PORT, () => console.log(`App available at http://localhost:${PORT}`));
+
 app.get('/', async (request, response) => {
     response.send( await readFile('./home.html', 'utf8') );
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
+const io = require('socket.io')(appServer, {
+    cors : { origin : '*' }
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
