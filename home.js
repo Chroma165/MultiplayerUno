7dea087dd4e4e84918bba48c6f599a5ac23a7ad8
Chroma165 <davidleutert@icloud.com>
2025-09-05 13:54:55 +0200

    Styling the home page

--- home.js
+++ home.js
@@ -3,3 +3,4 @@ const socket = io();
 const createRoom = document.querySelector('#createRoom');
 const joinRoom = document.querySelector('#joinRoom');
 const roomCodeInput = document.querySelector('#roomCodeInput');
+const userNameInput = document.querySelector('#userNameInput');
